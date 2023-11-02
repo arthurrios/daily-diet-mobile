9a22b26e1fbdd4c7365278ec7a7150e8b4bc1a89
arthur <arthur.rios007@gmail.com>
2023-11-01 21:59:06 -0300

    add stats page

---
+++ src/screens/Stats/index.tsx
@@ -0,0 +1,40 @@
+import { BalanceStyleProps } from '@components/Balance/styles'
+import { Arrow, Container, Main, Percentage, Subtitle, Title } from './styles'
+import { View } from 'react-native'
+import { ButtonIcon } from '@components/ButtonIcon'
+import { StatsCard } from '@components/StatsCard'
+import { HealthStatsCard } from '@components/HealthStatsCard'
+
+type Props = {
+  type?: BalanceStyleProps
+  percentage?: string
+}
+
+export function Stats({ percentage, type = 'HEALTHY' }: Props) {
+  return (
+    <Container type={type}>
+      <ButtonIcon icon={<Arrow type={type} />} />
+      <Percentage>90,86%</Percentage>
+      <Subtitle>of meals within diet</Subtitle>
+      <Main>
+        <Title>General stats</Title>
+        <View style={{ gap: 12 }}>
+          <StatsCard stats={22} subtitle="best dish sequence within diet" />
+          <StatsCard stats={109} subtitle="meals registered" />
+          <View style={{ flexDirection: 'row', gap: 12 }}>
+            <HealthStatsCard
+              stats={99}
+              subtitle="meals within diet"
+              type="HEALTHY"
+            />
+            <HealthStatsCard
+              stats={10}
+              subtitle="meals outside diet"
+              type="UNHEALTHY"
+            />
+          </View>
+        </View>
+      </Main>
+    </Container>
+  )
+}
---
+++ src/screens/Stats/styles.ts
@@ -0,0 +1,65 @@
+import styled, { css } from 'styled-components/native'
+import { BalanceStyleProps } from '@components/Balance/styles'
+import { ArrowLeft } from 'phosphor-react-native'
+
+type Props = {
+  type: BalanceStyleProps
+}
+
+export const Container = styled.View<Props>`
+  position: relative;
+  padding-top: 60px;
+  flex: 1;
+  background-color: ${({ theme, type }) =>
+    type === 'HEALTHY' ? theme.COLORS.GREEN_100 : theme.COLORS.RED_100};
+`
+
+export const Balance = styled.View<Props>`
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme, type }) =>
+    type === 'HEALTHY' ? theme.COLORS.GREEN_100 : theme.COLORS.RED_100};
+`
+
+export const Arrow = styled(ArrowLeft).attrs<Props>(({ theme, type }) => ({
+  size: 24,
+  color: type === 'HEALTHY' ? theme.COLORS.GREEN_800 : theme.COLORS.RED_800,
+}))`
+  position: absolute;
+  left: 32px;
+`
+
+export const Percentage = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.BOLD};
+    font-size: ${theme.FONT_SIZE.XXL}px;
+    color: ${theme.COLORS.GRAY_700};
+  `}
+  margin-top: 24px;
+  text-align: center;
+`
+
+export const Subtitle = styled.Text`
+  ${({ theme }) => css`
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.SM}px;
+    color: ${theme.COLORS.GRAY_600};
+  `}
+  text-align: center;
+  padding-bottom: 40px;
+`
+export const Main = styled.View`
+  padding: 33px 24px 0;
+  flex: 1;
+  border-radius: 20px;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_100};
+`
+export const Title = styled.Text`
+  ${({ theme }) => css`
+    color: ${theme.COLORS.GRAY_700};
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.SM}px;
+  `}
+  text-align: center;
+  margin-bottom: 23px;
+`
