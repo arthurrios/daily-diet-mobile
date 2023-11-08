6f280a2494a29c096b02fc359ecad408a4a32f4d
arthur <arthur.rios007@gmail.com>
2023-11-08 12:34:21 -0300

    fix get meals by date method

--- src/storage/meal/mealsGetByDate.ts
+++ src/storage/meal/mealsGetByDate.ts
@@ -1,11 +1,14 @@
 /* eslint-disable no-useless-catch */
+import { formatListDateToMoment } from '@utils/formatListDateToMoment'
 import { mealsGetAll } from './mealsGetAll'
 
 export async function mealsGetByDate(date: string) {
   try {
+    const formattedDate = formatListDateToMoment(date)
+
     const storage = await mealsGetAll()
 
-    const mealsSameDay = storage.filter((meal) => meal.date === date)
+    const mealsSameDay = storage.filter((meal) => meal.date === formattedDate)
 
     return mealsSameDay
   } catch (error) {
